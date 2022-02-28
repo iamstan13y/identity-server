2e85d217d65e1b1f9d0ddca0128412517a712a95
iamstan13y <iamstan13y@gmail.com>
2022-02-28 11:05:27 +0200

    Account controller.

---
+++ IdentityServer/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+       
+    }
+}
