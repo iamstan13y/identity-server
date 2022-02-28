6bfa3a0734a062bedbefd3a8281118ac8cd5b720
iamstan13y <iamstan13y@gmail.com>
2022-02-28 12:08:17 +0200

    Account Repository Interface.

---
+++ IdentityServer/Models/Repository/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityServer.Models.Repository
+{
+    public interface IAccountRepository
+    {
+        Task<Account> CreateAsync(Account account);
+        Task<Account> GetByIdAsync(int id);
+        Task<IEnumerable<Account>> GetAllAsync();
+        Task<Account> UpdateAsync(Account account);
+        Task<bool> DeleteAsync(Account account);
+
+    }
+}
