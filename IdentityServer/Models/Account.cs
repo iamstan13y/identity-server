e09cb85bfcf7a5cb548993a0e670fe4e8bd8ebd5
iamstan13y <iamstan13y@gmail.com>
2022-02-28 11:32:49 +0200

    Account model.

---
+++ IdentityServer/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IdentityServer.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public DateTime DateCreated { get; set; }
+        [NotMapped]
+        public string? Token { get; set; }
+
+    }
+}
